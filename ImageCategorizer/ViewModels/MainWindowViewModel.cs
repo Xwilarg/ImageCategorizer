ff9504dfd0ea4129405f448b89298262fb290c41
Xwilarg <xwilarg@yahoo.fr>
2021-07-10 02:59:22 +0200

    Add button code

--- ImageCategorizer/ViewModels/MainWindowViewModel.cs
+++ ImageCategorizer/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,27 @@
 using ReactiveUI;
+using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Windows.Input;
 
 namespace ImageCategorizer.ViewModels
 {
     public class MainWindowViewModel : ReactiveObject
     {
-        public string Greeting => "Welcome to Avalonia!";
+        public MainWindowViewModel()
+        {
+            LoadImage = ReactiveCommand.CreateFromTask(async () =>
+            {
+                await ShowSelectImageDialog.Handle(Unit.Default);
+            });
+        }
+
+        public Interaction<Unit, Unit> ShowSelectImageDialog { get; } = new();
+
+        public ICommand LoadImage { get; }
+        public ICommand Save { get; }
+        public ICommand Clear { get; }
+
+        public Guid Guid { private set; get; } = Guid.NewGuid();
     }
 }
--- ImageCategorizer/Views/MainWindow.axaml.cs
+++ ImageCategorizer/Views/MainWindow.axaml.cs
@@ -1,7 +1,14 @@
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 using Avalonia.ReactiveUI;
 using ImageCategorizer.ViewModels;
+using ReactiveUI;
+using System.Collections.Generic;
+using System.Reactive;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace ImageCategorizer.Views
 {
@@ -13,11 +20,44 @@ namespace ImageCategorizer.Views
 #if DEBUG
             this.AttachDevTools();
 #endif
+            this.WhenActivated(_ =>
+            {
+                ViewModel!.ShowSelectImageDialog.RegisterHandler(ShowSelectImageDialog);
+            });
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void Render(DrawingContext context)
+        {
+            this.FindControl<TextBlock>("ID").Text = ViewModel!.Guid.ToString();
+        }
+
+        private async Task ShowSelectImageDialog(InteractionContext<Unit, Unit> interaction)
+        {
+            var dialog = new OpenFileDialog
+            {
+                Filters = new List<FileDialogFilter>
+                {
+                    new FileDialogFilter
+                    {
+                        Name = "Image",
+                        Extensions = new() { "png", "jpg", "jpeg", "gif" }
+                    }
+                }
+            };
+            await dialog.ShowAsync((Window)VisualRoot).ContinueWith(async (s) =>
+            {
+                var files = await s.ConfigureAwait(false);
+                if (files.Length > 0)
+                {
+                    this.FindControl<Image>("PreviewImage").Source = new BitmapImage()
+                }
+            }).ConfigureAwait(false);
+            interaction.SetOutput(Unit.Default);
         }
     }
 }
